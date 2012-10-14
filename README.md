6118e9e0a1abf7a261cf0c30c48d08215eaed2be
C. Scott Ananian <cscott@cscott.net>
2012-10-13 22:40:12 -0400

    Basic first-draft implementation of needed API.

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ This is a very simple server side component to sync/save drawings from
 It is based on
 [apejs](http://lmatteis.github.com/apejs/), a tiny Javascript framework for
 Google App Engine.
+
+## API
+
+The journal is designed to mimic the [lawnchair](http://brian.io/lawnchair/)
+interface.  That's basically a key-value data store, where:
+- the key is a <db name, key> pair. get/set/exists methods.
+- values are arbitrary JSON objects
+- we can enumerate all the keys for a given db name
+  (results are paged to comply with appengine datastore restrictions)
+- we can delete all the keys for a given db name.
+  (may require multiple operations due to appengine datastore restrictions)
--- war/main.js
+++ war/main.js
@@ -1,134 +1,177 @@
 var apejs = require("apejs.js");
 var console = require("console.js");
-var select = require("select.js");
+var googlestore = require("googlestore.js");
 var version = require("version.js");
 
 var mustache = require("./common/mustache.js");
 
+var CORSHosts = [
+    'http://nell-colors.github.cscott.net',
+    'http://dev.laptop.org'
+];
+
+var BATCH_SIZE = 25; // items at a time.
+
+var ajaxResponse = function(response, callback) {
+    response.addHeader('Access-Control-Allow-Origin',
+                       CORSHosts.join(' '));
+    return {
+        write: function(value) {
+            var writer;
+            if (callback) { // JSONp
+                response.setContentType('text/javascript;charset=UTF-8');
+                writer = response.getWriter();
+                writer.print(callback);
+                writer.print('(');
+                writer.print(JSON.stringify(value));
+                writer.print(')');
+            } else { // regular JSON
+                response.setContentType('application/json;charset=UTF-8');
+                writer = response.getWriter();
+                writer.print(JSON.stringify(value));
+            }
+        }
+    };
+};
+var makeUserDbKey = function(uuid, dbname) {
+    var userKey = googlestore.createKey("User", uuid);
+    var dbKey = googlestore.createKey(userKey, "Database", dbname);
+    return dbKey;
+};
+var makeUserDbItemKey = function(uuid, dbname, key) {
+    var dbKey = makeUserDbKey(uuid, dbname);
+    var itemKey = googlestore.createKey(dbKey, "Item", key);
+    return itemKey;
+};
+
 apejs.urls = {
+    "/version": {
+        get: function(request, response, matches) {
+            var p = param(request);
+            var callback = p("callback"); // JSONP (optional)
+            ajaxResponse(response, callback).write(version);
+        }
+    },
+    "/(get|exists)/([a-zA-Z0-9_-]+)": {
+        get: function(request, response, matches) {
+            var getOrExists = matches[1];
+            var uuid = matches[2];
+            var p = param(request);
+            var dbname = p("dbname");
+            var key = p("key");
+            var callback = p("callback"); // JSONP (optional)
+
+            // fetch result
+            var itemKey = makeUserDbItemKey(uuid, dbname, key);
+            var item=null, found=false;
+            try {
+                item = googlestore.get(itemKey);
+                found = true;
+            } catch (e if e.javaException instanceof
+                     googlestore.EntityNotFoundException) {
+                found = false;
+            }
+            var result;
+            if (getOrExists==='get') {
+                result = item ? JSON.parse(item.getProperty('value')) : null;
+            } else { // exists
+                result = found;
+            }
+            ajaxResponse(response, callback).write(result);
+        }
+    },
+    "/(put|delete)/([a-zA-Z0-9_-]+)": {
+        post: function(request, response, matches) {
+            var putOrDelete = matches[1];
+            var uuid = matches[2];
+            var p = param(request);
+            var dbname = p("dbname");
+            var key = p("key");
+            var value = p("value"); // missing for delete, obviously
+            var entity;
+
+            // put or delete
+            if (putOrDelete==='put') {
+                JSON.parse(value); // validate.
+                entity = googlestore.entity("Item", key,
+                                            makeUserDbKey(uuid, dbname), {
+                    value: value
+                });
+                googlestore.put(entity);
+            } else { // delete
+                googlestore.del(makeUserDbItemKey(uuid, dbname, key));
+            }
+            ajaxResponse(response).write(true);
+        }
+    },
+    "/(keys|list)/([a-zA-Z0-9_-]+)": {
+        get: function(request, response, matches) {
+            var keysOrList = matches[1];
+            var uuid = matches[2];
+            var p = param(request);
+            var dbname = p("dbname");
+            var cursor = p("cursor"); // optional
+            var callback = p("callback"); // JSONP (optional)
+            // enumerate items
+            var userDbKey = makeUserDbKey(uuid, dbname);
+            var query = googlestore.query("Item").setAncestor(userDbKey);
+            if (keysOrList==='keys') { query.setKeysOnly(); }
+            if (cursor) { query.startCursor(cursor); }
+            var resultIter = query.fetchAsIterable(BATCH_SIZE).iterator();
+            var result = [], obj;
+            while (resultIter.hasNext()) {
+                var entity = resultIter.next();
+                var key = entity.getKey().getName();
+                if (keysOrList==='keys') {
+                    obj = key;
+                } else {
+                    obj = JSON.parse(entity.getProperty('value'));
+                    obj.key = key;
+                }
+                result.push(obj);
+            }
+            var endCursor = resultIter.getCursor.toWebSafeString();
+
+            ajaxResponse(response, callback).write({
+                cursor: endCursor,
+                result: result
+            });
+        }
+    },
+    "/nuke/([a-zA-Z0-9_-]+)": {
+        post: function(request, response, matches) {
+            var uuid = matches[1];
+            var p = param(request);
+            var dbname = p("dbname");
+            var cursor = p("cursor"); // optional
+            var userDbKey = makeUserDbKey(uuid, dbname);
+            var query = googlestore.query("Item").setAncestor(userDbKey);
+            query.setKeysOnly();
+            if (cursor) { query.startCursor(cursor); }
+            var resultIter = query.fetchAsIterable(BATCH_SIZE).iterator();
+            while (resultIter.hasNext()) {
+                var entity = resultIter.next();
+                googlestore.del(entity.getKey());
+            }
+            var endCursor = resultIter.getCursor.toWebSafeString();
+
+            ajaxResponse(response).write({
+                cursor: endCursor,
+            });
+        }
+    },
+
     "/": {
         get: function(request, response) {
             var p = param(request);
 
             var html = mustache.to_html(render("skins/index.html"), {
-                package: version.package,
-                version: version.number
+                package: version['package'],
+                version: version.number,
+                uuid: 'TESTME'
             });
             print(response).text(html);
-
-            select("person")
-                .find()
-                .sort("name", "ASC")
-                .each(function(id) {
-                    var person = mustache.to_html(render("skins/person.html"), {
-                        id: id,
-                        name: this["name"],
-                        age: this["age"],
-                        gender: this["gender"]
-                    });
-                    print(response).text(person);
-                });
-        },
-        post: function(request, response) {
-            var p = param(request);
-            select('person')
-                .add({
-                    "name"  : p("name"),
-                    "gender": p("gender"),
-                    "age":    parseInt(p("age"), 10),
-                    "jobs":   ["fanner", "fut", "fab"],
-                    "json":   {"foo":"bar"}
-                });
-            response.sendRedirect("/");
-        }
-    },
-    "/test" : {
-        get: function(request, response) {
-            var form = mustache.to_html(render("skins/form.html"));
-            print(response).text(form);
-
-            select("person")
-                .find()
-                .sort("name", "ASC")
-                .each(function(id) {
-                    var person = mustache.to_html(render("skins/person.html"), {
-                        id: id,
-                        name: this["name"],
-                        age: this["age"],
-                        gender: this["gender"]
-                    });
-                    print(response).text(person);
-                });
-        },
-        post: function(request, response) {
-            var par = param(request);
-
-            var form = mustache.to_html(render("skins/form.html"));
-            print(response).text(form);
-
-            var filter_by = par("filter_by");
-            // filter value can be string or number
-            var filter_val = par("filter_val");
-
-            // note the hacky type conversions: par(...) does not yield a
-            // string directly, and numeric filters must have a number.
-            filter_by = '' + filter_by;
-            filter_val = '' + filter_val;
-            if (filter_by==='age') {
-                filter_val = +filter_val;
-            }
-            // create select options
-            var filter = {};
-            filter[filter_by + par("filter_op")] = filter_val;
-
-
-            select("person")
-                .find(filter)
-                .sort(par("filter_by"))
-                .sort(par("sort_by"), par("sort_dir"))
-                .each(function(id) {
-                    var person = mustache.to_html(render("skins/person.html"), {
-                        id: id,
-                        name: this["name"],
-                        age: this["age"],
-                        gender: this["gender"]
-                    });
-                    print(response).text(person);
-                });
-        }
-    },
-    "/person/([a-zA-Z0-9_]+)" : {
-        get: function(request, response, matches) {
-            var id     = parseInt(matches[1], 10);
-            select("person")
-                .find(id)
-                .each(function(id) {
-                    var person = mustache.to_html(render("skins/person.html"), {
-                        id: id,
-                        name: this["name"],
-                        age: this["age"],
-                        gender: this["gender"]
-                    });
-                    print(response).text(person);
-                });
-        }
-    },
-    "/edit/([0-9]+)" : {
-        get: function(request, response, matches) {
-            var id  = parseInt(matches[1], 10);
-            select("person")
-                .find(id)
-                .attr({name: "Name Has Been Edited"});
-            response.sendRedirect("/");
-        }
-    },
-    "/delete/([0-9]+)" : {
-        get: function(request, response, matches) {
-            var id  = parseInt(matches[1], 10);
-            select("person")
-                .find(id)
-                .del();
-            response.sendRedirect("/");
+            // XXX other info?
         }
     }
 };
